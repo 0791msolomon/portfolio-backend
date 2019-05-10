156c3569993da460ca4d8b16232b35988b89569c
Matt <matthew@realtimereviews.com>
2019-05-10 14:00:03 -0700

    added response header to http requests

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(router);
 
 mongoose.connect(url, { useNewUrlParser: true }, function(err) {
