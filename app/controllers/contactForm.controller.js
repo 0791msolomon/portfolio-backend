6dbe7a7bf359662f28871850205f46d34c78968e
Matt <matthew@realtimereviews.com>
2019-05-10 13:43:12 -0700

    added in mailgun call

--- app/controllers/contactForm.controller.js
+++ app/controllers/contactForm.controller.js
@@ -1,19 +1,44 @@
 const { Contact } = require("../models/Contact");
+var mailgun = require("mailgun-js");
+var DOMAIN = "mg.mattsolomondomain.com";
+var mailgun = require("mailgun-js")({
+  apiKey: process.env.REACT_APP_MAILGUN_KEY,
+  domain: DOMAIN
+});
 
 const createContact = (req, res) => {
-  let contact = new Contact({
-    name: req.body.name,
-    company: req.body.company,
-    email: req.body.email
-  });
-  contact
-    .save()
+  // let contact = new Contact({
+  //   name: req.body.name,
+  //   company: req.body.company,
+  //   email: req.body.email
+  // });
+  // contact
+  //   .save()
+  //   .then(response => {
+  //     res.status(201).send(response);
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //     res.status(500).send(err);
+  //   });
+  var data = {
+    from: "0791msolomon@gmail.com",
+    to: "0791msolomon@gmail.com",
+    subject: `${req.body.company} is Interested in you`,
+    text: `${req.body.name} is interested in you for ${
+      req.body.company
+    }, reply back to them at ${req.body.email}`
+  };
+  mailgun
+    .messages()
+    .send(data)
     .then(response => {
-      res.status(201).send(response);
+      console.log(response);
+      res.status(200).send(response);
     })
     .catch(err => {
       console.log(err);
-      res.status(500).send(err);
+      res.status(400).send(err);
     });
 };
 module.exports = { createContact };
